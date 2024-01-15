886ccc7424f094e74c4a32aedca4db78f1e2e5b5
OMGBEN <156272837+OMGBEN@users.noreply.github.com>
2024-01-15 11:24:43 +1100

    Update MSO - Optermisation Engine Connected.R

--- MSO - Optermisation Engine Connected.R
+++ MSO - Optermisation Engine Connected.R
@@ -104,12 +104,7 @@ best_index <- which.max(sapply(net_reach_values_list, function(result) result$ne
 # Access the corresponding result
 best_result <- net_reach_values_list[[best_index]]
 
-# Create a data frame from the first column of abc_curves and add it to channel_allocation_opt
-#channel_allocation_opt <- data.frame(Channel = abc_curves$Key, best_result$opt_budget_split)
-channel_allocation_opt <- data.frame(Budget = as.numeric(best_result$opt_budget_split))
 
-# Assign channel_allocation_opt to the global environment
-assign("channel_allocation_opt", channel_allocation_opt, envir = .GlobalEnv)
 
 # Display the best result
 cat("Best Random Allocation:", best_result$random_allocation, "\n")
